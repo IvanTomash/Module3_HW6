4676270b6f142ced49e45f7a39ddb2526c02aec7
IvanTomash <tomashuckvanya@gmail.com>
2023-09-06 13:33:26 +0300

    First commit HW_3_6

---
+++ Homework_3_6/Homework_3_6/MessageBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3_6
+{
+    internal sealed class MessageBox
+    {
+        public Action<State> WindowIsClosed;
+
+        public MessageBox() 
+        {
+        }
+
+        public async void Open()
+        {
+            Console.WriteLine("The window is open");
+            await Task.Delay(3000);
+            Console.WriteLine("The window was closed by the user");
+            WindowIsClosed.Invoke(GetRandomState());
+        }
+
+        private State GetRandomState()
+        {
+            Random rnd = new Random();
+            return (State)rnd.Next(0, 2);
+        }
+    }
+}
---
+++ Homework_3_6/Homework_3_6/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3_6
+{
+    internal enum State
+    {
+        Ok,
+        Cancel
+    }
+}
